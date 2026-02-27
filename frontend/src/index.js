376eba6abe32fae4229d2765943496e653e04985
Summayyah Memon <summayyah_memon@sfu.ca>
2026-02-27 14:16:19 -0800

    added in fonts, global styling, and played around with login.js screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./assets/styles/fonts.css";
+import "./assets/styles/global.css";
+import "./App.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
