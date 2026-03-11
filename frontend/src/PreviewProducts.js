6871ff4e1952f7be7478aae3c38564269e49ac5e
Therese <twa140@sfu.ca>
2026-03-11 00:12:16 -0700

    Update PreviewProducts.js

--- frontend/src/PreviewProducts.js
+++ frontend/src/PreviewProducts.js
@@ -4,7 +4,6 @@ import "./App.css";
 function PreviewProducts() {
   const [allProducts, setAllProducts] = useState([]); // store everything for filtering
   const [searchTerm, setSearchTerm] = useState("");
-  const [product, setProduct] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/products")
@@ -16,11 +15,6 @@ function PreviewProducts() {
       })
       .catch((err) => console.error("Error fetching public products:", err));
   }, []);
-
-  // split name and amount just like in Dashboard
-  const nameParts = product.productName.split(",");
-  const amount = nameParts.length > 1 ? nameParts.pop().trim() : "";
-  const cleanName = nameParts.join(",").trim();
 
   return (
     <div className="page-container bkgd-blue">
@@ -44,25 +38,36 @@ function PreviewProducts() {
           </div>
         </div>
 
-        <div key={product._id} className="product-card">
-          <div className="image-container">
-            {product.imageUrl ? (
-              <img src={product.imageUrl} alt={product.productName} />
-            ) : (
-              <div className="placeholder">No Image</div>
-            )}
-          </div>
-          <div className="card-details">
-            <h3 className="h3-ivy">{product.brand}</h3>
-            <h3>{cleanName}</h3>
-            {amount && <p>{amount}</p>}
-            <p>
-              <strong>Usage Type:</strong> {product.usageType}
-            </p>
-            <p>
-              <strong>Category:</strong> {product.category}
-            </p>
-          </div>
+        <div className="preview-grid">
+          {allProducts.slice(0, 10).map((product) => {
+            const nameParts = product.productName.split(",");
+            const amount = nameParts.length > 1 ? nameParts.pop().trim() : "";
+            const cleanName = nameParts.join(",").trim();
+
+            return (
+              <div key={product._id} className="product-card">
+                <div className="image-container">
+                  {product.imageUrl ? (
+                    <img src={product.imageUrl} alt={product.productName} />
+                  ) : (
+                    <div className="placeholder">No Image</div>
+                  )}
+                </div>
+
+                <div className="card-details">
+                  <h3 className="h3-ivy">{product.brand}</h3>
+                  <h3>{cleanName}</h3>
+                  {amount && <p>{amount}</p>}
+                  <p>
+                    <strong>Usage Type:</strong> {product.usageType}
+                  </p>
+                  <p>
+                    <strong>Category:</strong> {product.category}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
