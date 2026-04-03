a7f522bd7910ba557293b062175b254eb1fc8b7c
Summayyah Memon <summayyah_memon@sfu.ca>
2026-04-02 23:04:01 -0700

    implemented ui cards styling across application, fixed filtering

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -312,7 +312,7 @@ function Dashboard() {
         `http://localhost:5000/api/products?skip=${skip}&limit=${limit}`,
       );
       const data = await res.json();
-      setProducts((prev) => [...prev, ...data]); // append to existing products
+      setProducts(data);
     } catch (err) {
       console.error(err);
     }
@@ -515,24 +515,22 @@ function Dashboard() {
                           </span>
                         )}
                       </div>
-              
+
                       <div className="button-group">
                         <Link to={`/products/${product._id}`}>
                           <button>See Details</button>
                         </Link>
 
-                        {
-                          <button onClick={() => saveProduct(product._id)}>
-                            Save to Favourites
-                          </button>
-                        }
+                        {/* <button onClick={() => saveProduct(product._id)}>
+                          Save to Favourites
+                        </button> */}
 
-                        {/* <button
+                        <button
                           className="heart-button"
                           onClick={() => saveProduct(product._id)}
                         >
                           <FaHeart />
-                        </button> */}
+                        </button>
                       </div>
                     </div>
                   </div>
